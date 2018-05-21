0b1a96f3881b816b5921c990b9d893b92bfd0997
Austin Roberts <d.austin.roberts@gmail.com>
2018-05-20 18:54:44 -0700

    Adding and debugging front end, still some errors

---
+++ band_tracker.sql
@@ -0,0 +1,116 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: May 21, 2018 at 01:47 AM
+-- Server version: 5.6.38
+-- PHP Version: 7.2.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `band_tracker`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bands`
+--
+
+CREATE TABLE `bands` (
+  `id` int(11) NOT NULL,
+  `band_name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `bands`
+--
+
+INSERT INTO `bands` (`id`, `band_name`) VALUES
+(1, 'tol'),
+(2, 'Moby'),
+(3, 'Red Hot Chili Peppers'),
+(4, 'Moby');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bands_venues`
+--
+
+CREATE TABLE `bands_venues` (
+  `id` int(11) NOT NULL,
+  `bands_id` int(11) NOT NULL,
+  `venues_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `venues`
+--
+
+CREATE TABLE `venues` (
+  `id` int(11) NOT NULL,
+  `venue_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `venues`
+--
+
+INSERT INTO `venues` (`id`, `venue_name`) VALUES
+(7, 'Portland'),
+(8, '7'),
+(9, '7');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bands`
+--
+ALTER TABLE `bands`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `bands_venues`
+--
+ALTER TABLE `bands_venues`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `venues`
+--
+ALTER TABLE `venues`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bands`
+--
+ALTER TABLE `bands`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `venues`
+--
+ALTER TABLE `venues`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
